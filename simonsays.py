97f28bd59091312f1401a912d6989d1f6e306e69
beor20 <beor20@gmail.com>
2018-07-24 13:45:55 -0700

    adds random colors and freqencies

--- simonsays.py
+++ simonsays.py
@@ -8,17 +8,34 @@ R_pin = 11
 G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
+# this script plays one of four random notes ob the passive buzzer
+# breadboardsetup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+#set Passive Buzzer pin's mode as output
+GPIO.setup(buzz_pin, GPIO.OUT)
+Buzz = GPIO.PWM(buzz_pin,1000)
+frequences = [220, 440, 880, 1760]
 
 def loop():
+	n = random.randint(0,3)
+	color_sequence = [colors[n]]
 	while True:
+		for i in range(0, len(color_sequence)):
+			Buzz.ChangeFrequency(frequencies[n])
+			Buzz.start(50)
+			time.sleep(0.5)
+			LED.setColor(color_sequence[n])
+			Buzz.stop()
+			time.sleep(0.5)
+			LED.noColor()
+			time.sleep(0.2)
 		n = random.randint(0,3)
-		LED.setColor(colors[n])
-		time.sleep(0.5)
-		LED.noColor()
-		time.sleep(0.5)
-
+		color_sequence. append(colors[n])
+		time.sleep(0.3)
 if __name__ == '__main__':
 	try:
 		loop()
 	except KeyboardInterrupt:		
 		print 'Good bye'
+		LED.destroy()
